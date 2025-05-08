c88f6ad640d2644836d8065cd450ef789e1bb82b
Andrei Pronsky <95336511+AndreiPronsky@users.noreply.github.com>
2025-05-08 21:56:49 +0300

    Tests for UserService performed

--- src/main/java/org/pronsky/subscriptionservice/service/dto/request/CreateUserRequestDto.java
+++ src/main/java/org/pronsky/subscriptionservice/service/dto/request/CreateUserRequestDto.java
@@ -1,13 +1,17 @@
 package org.pronsky.subscriptionservice.service.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserRequestDto {
 
     @NotBlank
